e42cd6e9e7c45104dc6e61b47a66fe47fde61166
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-05-24 17:39:05 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,140 @@
+const viewSection = document.querySelector(".view-section");
+const contactsSection = document.querySelector(".contacts-section");
+
+const state = {
+  contacts: [],
+  selectedContact: null
+};
+
+/* [START] NO NEED TO EDIT */
+
+function getContacts() {
+  fetch("http://localhost:3000/contacts")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      state.contacts = data;
+
+      renderContactsList();
+    });
+}
+
+function renderContactsList() {
+  const listEl = document.createElement("ul");
+  listEl.className = "contacts-list";
+
+  for (let i = 0; i < state.contacts.length; i++) {
+    const contact = state.contacts[i];
+    const listItemEl = renderContactListItem(contact);
+
+    listEl.append(listItemEl);
+  }
+
+  contactsSection.append(listEl);
+}
+
+function renderAddressSection(address) {
+  const containerEl = document.createElement("section");
+
+  const headingEl = document.createElement("h2");
+  headingEl.innerText = "Address";
+
+  containerEl.append(headingEl);
+
+  const streetText = document.createElement("p");
+  streetText.innerText = address.street;
+
+  containerEl.append(streetText);
+
+  const cityText = document.createElement("p");
+  cityText.innerText = address.city;
+
+  containerEl.append(cityText);
+
+  const postCodeText = document.createElement("p");
+  postCodeText.innerText = address.postCode;
+
+  containerEl.append(postCodeText);
+
+  return containerEl;
+}
+
+function renderContactView() {
+  const contact = state.selectedContact;
+
+  if (!contact) return;
+
+  viewSection.innerHTML = "";
+
+  const containerEl = document.createElement("article");
+  containerEl.className = "center light-shadow address-card";
+
+  const headingEl = document.createElement("h1");
+
+  const fullName = `${contact.firstName} ${contact.lastName}`;
+  headingEl.innerText = fullName;
+
+  containerEl.append(headingEl);
+
+  const addressSectionEl = renderAddressSection(contact.address);
+
+  containerEl.append(addressSectionEl);
+
+  viewSection.append(containerEl);
+}
+
+/* [END] NO NEED TO EDIT */
+
+function renderContactListItem(contact) {
+  const listItemEl = document.createElement("li");
+
+  const headingEl = document.createElement("h3");
+
+  const fullName = `${contact.firstName} ${contact.lastName}`;
+
+  headingEl.innerText = fullName;
+
+  listItemEl.append(headingEl);
+
+  const viewBtn = document.createElement("button");
+  viewBtn.className = "button grey";
+  viewBtn.innerText = "View";
+
+  viewBtn.addEventListener("click", function () {
+    state.selectedContact = contact;
+
+    renderContactView();
+  });
+
+  listItemEl.append(viewBtn);
+
+  const editBtn = document.createElement("button");
+  editBtn.className = "button blue";
+  editBtn.innerText = "Edit";
+
+  editBtn.addEventListener("click", function () {
+    // [TODO] Write Code
+  });
+
+  listItemEl.append(editBtn);
+
+  return listItemEl;
+}
+
+function listenNewContactButton() {
+  const btn = document.querySelector(".new-contact-btn");
+
+  btn.addEventListener("click", function () {
+    // [TODO] Write Code
+  });
+}
+
+// [TODO] Write Code
+
+function main() {
+  listenNewContactButton();
+  getContacts();
+}
+
+main();
