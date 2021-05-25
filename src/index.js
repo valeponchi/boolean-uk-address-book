c651700563ab368f0be9a63874bb611280acfd80
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-05-26 00:31:23 +0100

    showing form, working on it, and now is giving me 500 error.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+// Create Element function shortcut
+// function createEl(tag, attobj) {
+//   const elm = document.createElement(tag);
+//   for (const key of Object.keys(attobj)) {
+//     elm[key] = attobj[key];
+//   }
+//   return elm;
+// }
+
 const viewSection = document.querySelector(".view-section");
 const contactsSection = document.querySelector(".contacts-section");
+const mainEl = document.querySelector(".view-section")
 
 const state = {
   contacts: [],
@@ -15,6 +25,7 @@ function getContacts() {
     })
     .then(function (data) {
       state.contacts = data;
+      console.log(state.contacts)
 
       renderContactsList();
     });
@@ -123,12 +134,164 @@ function renderContactListItem(contact) {
 }
 
 function listenNewContactButton() {
-  const btn = document.querySelector(".new-contact-btn");
+
+  let btn = document.querySelector(".new-contact-btn");
 
   btn.addEventListener("click", function () {
     // [TODO] Write Code
-  });
-}
+    formEl = document.createElement(`form`)
+    formEl.setAttribute(`class`, `form-stack`)
+    formEl.classList.add( `light-shadow`, `center`, `contact-form`)
+
+    h1El = document.createElement(`h1`)
+    h1El.innerText = "Create contact" 
+
+    // FIRST NAME
+    labelFirstNameEl = document.createElement(`label`)
+    labelFirstNameEl.setAttribute(`for`, `first-name-input`)
+    labelFirstNameEl.innerText = "First Name:"
+    
+    inputFirstNameEl = document.createElement(`input`)
+    inputFirstNameEl.setAttribute(`id`, `first-name-input`)
+    inputFirstNameEl.setAttribute(`name`, `first-name-input`)
+    inputFirstNameEl.setAttribute(`type`, `text`)
+
+    //LAST NAME
+    labelLastNameEl = document.createElement(`label`)
+    labelLastNameEl.setAttribute(`for`, `last-name-input`)
+    labelLastNameEl.innerText = "Last Name:"
+
+    inputLastNameEl = document.createElement(`input`)
+    inputLastNameEl.setAttribute(`id`, `last-name-input`)
+    inputLastNameEl.setAttribute(`name`, `last-name-input`)
+    inputLastNameEl.setAttribute(`type`, `text`)
+
+    //STREET
+    labelStreetEl = document.createElement(`label`)
+    labelStreetEl.setAttribute(`for`, `street-input`)
+    labelStreetEl.innerText = "street:"
+
+    inputStreetEl = document.createElement(`input`)
+    inputStreetEl.setAttribute(`id`, `street-input`)
+    inputStreetEl.setAttribute(`name`, `street-input`)
+    inputStreetEl.setAttribute(`type`, `text`)
+
+    //CITY
+    labelCityEl = document.createElement(`label`)
+    labelCityEl.setAttribute(`for`, `city-input`)
+    labelCityEl.innerText = "City:"
+
+    inputCityEl = document.createElement(`input`)
+    inputCityEl.setAttribute(`id`, `city-input`)
+    inputCityEl.setAttribute(`name`, `city-input`)
+    inputCityEl.setAttribute(`type`, `text`)
+
+    //POST CODE
+    labelPostcodeEl = document.createElement(`label`)
+    labelPostcodeEl.setAttribute(`for`, `post-code-input`)
+    labelPostcodeEl.innerText = "Post Code:"
+
+    inputPostcodeEl = document.createElement(`input`)
+    inputPostcodeEl.setAttribute(`id`, `post-code-input`)
+    inputPostcodeEl.setAttribute(`name`, `post-code-input`)
+    inputPostcodeEl.setAttribute(`type`, `text`)
+
+    //CHECKBOX SECTION
+    checkboxSection = document.createElement(`div`)
+    checkboxSection.setAttribute(`class`, `checkbox-section`)
+
+    //BLOCK CHECKBOX
+    blockInputEl = document.createElement(`input`)
+    blockInputEl.setAttribute(`id`, `block-checkbox`)
+    blockInputEl.setAttribute(`name`, `block-checkbox`)
+    blockInputEl.setAttribute(`type`, `checkbox`)
+
+    blockLabelEl = document.createElement(`label`)
+    blockLabelEl.setAttribute(`for`, `block-checkbox`)
+    blockLabelEl.innerText = "Block"
+
+    //ACTION SECTION
+    actionSectionEl = document.createElement(`div`)
+    actionSectionEl.setAttribute(`class`, `actions-section`)
+
+    //ACTION BUTTON
+    btnCreate = document.createElement(`button`)
+    btnCreate.setAttribute(`class`, `button`)
+    btnCreate.classList.add(`blue`)
+    btnCreate.innerText = "Block"
+    btnCreate.setAttribute(`type`, `submit`)
+    btnCreate.innerText = "Create"
+
+    formEl.addEventListener(`submit`, function(e) {
+      e.preventDefault()
+      // add contact to State
+      // update SERVER
+      // call f render
+      
+      let newContact = {
+        blockContact: false,
+        firstName: formEl["first-name-input"].value,
+        lastName: formEl["last-name-input"].value,
+        // addressId: ????
+      }
+
+      let newAddress = {
+          city: formEl["city-input"].value,
+          postCode: formEl["post-code-input"].value,
+          street: formEl["street-input"].value
+        }
+
+      fetch("http://localhost:3000/contacts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newContact),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Now contacts-data in Server are", data);
+        });
+    
+      fetch("http://localhost:3000/addresses", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newAddress),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+        console.log("Now addresses-data in Server are", data);
+        });
+      const contactsListEl = document.querySelector(".contacts-list");
+      contactsListEl.innerHTML = "";
+      main();      
+      }//f of 2nd argument
+    ) //submit arguments
+
+    //APPENDING:
+    actionSectionEl.append(btnCreate)
+
+    checkboxSection.append(blockInputEl, blockLabelEl)
+
+    formEl.append(h1El, 
+      labelFirstNameEl, 
+      inputFirstNameEl, 
+      labelLastNameEl, 
+      inputLastNameEl,
+      labelStreetEl,
+      inputStreetEl,
+      labelCityEl,
+      inputCityEl,
+      labelPostcodeEl,
+      inputPostcodeEl,
+      checkboxSection,
+      actionSectionEl)
+
+    mainEl.append(formEl)
+  })//eventListener
+} //function
 
 // [TODO] Write Code
 
